f12edfb653f9bcf3de7e4381ea22f5591ccdbe28
Mike Cuddy <mcuddy77@gmail.com>
2016-01-03 13:48:21 -0500

    working on the explore method

--- adven.rb
+++ adven.rb
@@ -49,7 +49,24 @@ def explore(life)
 	puts `clear`
 	puts "you get up off of the bed and start to look around the room"
 	puts "It is an empy room with a shelf across from you."
-	puts "You walk "
+	puts "You walk to the shelf which is very dusty"
+	puts "on the shelf you notice that there is a key"
+	puts "Do you want to take the key? (yes/no)"
+	item = []
+	choice = gets.chomp.downcase
+	if choice == 'yes'
+		item.push('key')
+		puts 'You have taking the key and it is added to your inventory'
+		puts "#{item[0]} is in your inventory"
+	elsif choice == 'no'
+		puts "You decide not to take the key"
+	end 
+	puts "You turn around to see a small door next to you"
+	puts "You reach out and try turning the door handle but it is locked"
+	puts "Will you try opening the door?"
+	choice = gets.chomp 
+	if 
+		
 end 
 
 main 
