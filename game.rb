16290ca2210e4acd863e7ce9cdafc8ac65394784
ravenusmc <mcuddy77@gmail.com>
2016-02-21 16:15:56 -0500

    working on ruby methods

---
+++ game.rb
@@ -0,0 +1,23 @@
+def main 
+  puts "Welcome to Mikes game collection"
+  puts "--------------------------------"
+  puts "What would you like to do?"
+  puts "1. Open an account"
+  puts "2. Exit the program"
+
+  choice = gets.chomp.to_i
+  if choice == 1
+    open
+  end 
+end 
+
+def open
+  account = []
+  puts "You have opened an account."
+  puts "What would you like to name the account?"
+
+  name = gets.chomp.to_s
+  account.push(name)
+end 
+
+main 
