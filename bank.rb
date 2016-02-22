088f8a64f73249a6f447b86a9be274a5cc157ae7
ravenusmc <mcuddy77@gmail.com>
2016-02-22 10:12:52 -0500

    worked on deposit method

--- bank.rb
+++ bank.rb
@@ -13,7 +13,19 @@ end
 
 def deposit
   puts "How much would you like to deposit?"
-
+  balance = 0 
+  add_money = gets.chomp.to_i 
+  balance = balance + add_money  
+  puts "Would you like to deposit more money?(y/n)"
+  choice = gets.chomp.to_s
+  while choice != 'n'
+    puts "How much would you like to deposit?"
+    add_money = gets.chomp.to_i
+    balance = balance + add_money 
+    puts "Would you like to deposit more money?(y/n)"
+    choice = gets.chomp.to_s
+  end 
+  puts balance
 end 
 
 main 
