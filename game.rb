7668950920c948453cf173ff591e0da97276e616
ravenusmc <mcuddy77@gmail.com>
2016-02-27 16:38:45 -0500

    working on game program

--- game.rb
+++ game.rb
@@ -5,19 +5,22 @@ def main
   puts "1. Open an account"
   puts "2. Exit the program"
 
+  account = []
+
   choice = gets.chomp.to_i
   if choice == 1
     open
   end 
 end 
 
-def open
-  account = []
+def open(account)
   puts "You have opened an account."
   puts "What would you like to name the account?"
 
   name = gets.chomp.to_s
   account.push(name)
+  puts account 
+
 end 
 
 main 
