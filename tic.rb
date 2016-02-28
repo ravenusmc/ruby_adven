43f5a0da899904aa9a21cb55837f206c7dd537a7
ravenusmc <mcuddy77@gmail.com>
2016-02-28 17:34:15 -0500

    working on tic tac toe game

--- tic.rb
+++ tic.rb
@@ -1,1 +1,10 @@
-tic.rb
+def display_board(boar)
+  puts "   |   |   "
+  puts "___________"
+  puts "   |   |   "
+  puts "___________"
+  puts "   |   |   "
+end 
+
+board = ["  "," "," "," "," "," "," "," "," "]
+display_board(board)
