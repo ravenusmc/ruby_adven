0545b317d455af29b9633bc966519e97f52cacc7
ravenusmc <mcuddy77@gmail.com>
2016-02-29 14:11:19 -0500

    working on tic tac toe board

--- tic.rb
+++ tic.rb
@@ -1,10 +1,17 @@
 def display_board(board)
-  puts " #{board[0]}  |   |   "
+  puts " #{board[0]} |#{board[1]}|#{board[2]}"
   puts "___________"
-  puts "   |   |   "
+  puts " #{board[3]} |#{board[4]}|#{board[5]}"
   puts "___________"
-  puts "   |   |   "
+  puts " #{board[6]} |#{board[7]}|#{board[8]}"
 end 
 
-board = ["X", " ", " ", " ", " ", " ", " ", " ", " "]
+board = ["X "," O "," X ","  ","  ","   ","  ","  ","  "]
 display_board(board)
+
+# puts "Where do you want to place a mark?"
+# puts "please choose a number 1-9"
+# choice = gets.chomp.to_i
+# if choice == 1 
+#   board[1].push(X)
+# end 
