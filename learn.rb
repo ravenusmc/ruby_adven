3a5cb44c2c030ddd234cf6a5e165dbf8e38d00f0
ravenusmc <mcuddy77@gmail.com>
2016-03-05 16:53:35 -0500

    OOP work

--- learn.rb
+++ learn.rb
@@ -1,25 +1,18 @@
-def main
-  puts "Please give me a number"
-  number = gets.chomp.to_i
-  puts "What would you like to do with the number?"
-  puts "1. Add Two to number"
-  puts "2. Add three to number"
-  choice = gets.chomp.to_i
-  if choice == 1
-    addTwo(number)
-  elsif choice == 2
-    second_choice
-  end 
-end
+class Car 
 
-def addTwo(number)
-  puts `clear`
-  puts "adding two to number"
-  number = number + 2
-  puts number
+  def horn
+    puts "beep beep"
+  end 
+
+  def increase
+    puts "speed is increased"
+  end 
+
 end 
 
+car = Car.new
 
-main
+car.horn 
+
 
 
