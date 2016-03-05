aef3774dc076cd113fa5c793de510914fa652663
ravenusmc <mcuddy77@gmail.com>
2016-03-05 16:22:01 -0500

    palidrome test

--- prob.rb
+++ prob.rb
@@ -1,26 +1,43 @@
 #Max Min problem-get a series of numbers and then tell the user the max and min numbers 
 #entered into the program. 
 
-puts "Please enter a number"
-num = gets.chomp.to_i
-max = num 
-min = num 
+# puts "Please enter a number"
+# num = gets.chomp.to_i
+# max = num 
+# min = num 
 
-while num != -99 
-  puts "Please enter a number"
-  num = gets.chomp.to_i
+# while num != -99 
+#   puts "Please enter a number"
+#   num = gets.chomp.to_i
 
-  if num == -99
-  elsif num > max 
-    max = num  
-  elsif num < min 
-    min = num 
+#   if num == -99
+#   elsif num > max 
+#     max = num  
+#   elsif num < min 
+#     min = num 
+#   end 
+# end 
+
+# puts max 
+# puts min 
+
+#Way to reverse a  string and test iff it is a palidrome 
+# puts "Please give me a word to see if it is a palidrome"
+# word = gets.chomp
+
+# if word == word.reverse 
+#   puts "word is a palidrome"
+# else 
+#   puts "word is not a palidrome"
+# end 
+
+#Same exercise with a method
+def palidrome(word)
+  if word == word.reverse 
+    puts "Word is a palidrome"
+  else
+    puts "word is not a palidrome"
   end 
 end 
 
-puts max 
-puts min 
-
-#Way to reverse a  string and test iff it is a palidrome 
-puts "Please give me a word to see if it is a palidrome"
-word = gets.chomp
+palidrome("Mike")
