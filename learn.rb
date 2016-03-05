5b512fc41e7cd3f4312a89005b2fda8205dfbf21
ravenusmc <mcuddy77@gmail.com>
2016-03-05 16:26:05 -0500

    created a new file

---
+++ learn.rb
@@ -0,0 +1,4 @@
+puts "Please give me a number"
+number = gets.chomp.to_i
+
+
--- prob.rb
+++ prob.rb
@@ -1,5 +1,5 @@
-#Max Min problem-get a series of numbers and then tell the user the max and min numbers 
-#entered into the program. 
+############ Max Min problem-get a series of numbers and then tell the user the max and min numbers 
+#entered into the program. #######################
 
 # puts "Please enter a number"
 # num = gets.chomp.to_i
@@ -21,7 +21,7 @@
 # puts max 
 # puts min 
 
-#Way to reverse a  string and test iff it is a palidrome 
+################## Way to reverse a  string and test iff it is a palidrome #################
 # puts "Please give me a word to see if it is a palidrome"
 # word = gets.chomp
 
@@ -31,13 +31,13 @@
 #   puts "word is not a palidrome"
 # end 
 
-#Same exercise with a method
-def palidrome(word)
-  if word == word.reverse 
-    puts "Word is a palidrome"
-  else
-    puts "word is not a palidrome"
-  end 
-end 
+################## Same exercise with a method #########################
+# def palidrome(word)
+#   if word == word.reverse 
+#     puts "Word is a palidrome"
+#   else
+#     puts "word is not a palidrome"
+#   end 
+# end 
 
-palidrome("Mike")
+# palidrome("Mike")
