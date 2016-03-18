a54ca11027ac00987d190e048d1f013f171b5bbc
ravenusmc <mcuddy77@gmail.com>
2016-03-18 14:13:24 -0400

    added methods to my game

--- rock.rb
+++ rock.rb
@@ -1,27 +1,32 @@
 #Rock Paper Sciccors game. 
 
-
-#Human selects their weapon 
-puts "Welcome to Paper, Rock Scissors"
-puts "Please select what you want to use"
-puts "*********************************"
-puts "1. Rock"
-puts "2. Scissors"
-puts "3. Paper"
-puts "What is your choice?"
-choice = gets.chomp.to_i
-if choice == 1
-  weapon = "Rock"
-elsif choice == 2
-  weapon = "Scissors"
-elsif choice == 3
-  weapon = "Paper"
+def main 
+  puts "Welcome to Paper, Rock Scissors"
+  human
 end 
 
-puts "you selected #{weapon} for your choice!"
+#Human selects their weapon 
+def human
+  puts "Please select what you want to use"
+  puts "*********************************"
+  puts "1. Rock"
+  puts "2. Scissors"
+  puts "3. Paper"
+  puts "What is your choice?"
+  choice = gets.chomp.to_i
+  if choice == 1
+    weapon = "Rock"
+  elsif choice == 2
+    weapon = "Scissors"
+  elsif choice == 3
+    weapon = "Paper"
+  end 
+  puts "you selected #{weapon} for your choice!"
+  robot(weapon)
+end
 
 #AI Selects there weapon 
-
+def robot(weapon)
   if rand(1..3) == 1
     tool = "Rock"
   elsif rand(1..3) == 2
@@ -29,11 +34,12 @@ puts "you selected #{weapon} for your choice!"
   elsif rand(1..3) == 3
     tool = "Paper"
   end 
-
-puts "The computer selected #{tool} as their weapon!"
+  puts "The computer selected #{tool} as their weapon!"
+  battle(weapon,tool)
+end 
 
 #The code for the battle:
-
+def battle(weapon, tool)
 # while tool != weapon do 
   if tool == "Rock" && weapon == "Scissors"
       puts "The computer wins"
@@ -49,10 +55,11 @@ puts "The computer selected #{tool} as their weapon!"
       puts "The Human wins"
     elsif tool == weapon 
       puts "Tie Game!"
+  # end   
   end 
-# end 
+end 
 
-
+main 
 
  
 
