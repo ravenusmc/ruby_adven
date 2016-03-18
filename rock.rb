509742c363c4bb9675479c70c6bbe895438efba9
ravenusmc <mcuddy77@gmail.com>
2016-03-18 13:23:22 -0400

    finished conditional statement

--- rock.rb
+++ rock.rb
@@ -1,6 +1,21 @@
 #Rock Paper Sciccors game. 
 
-puts "Welcome to Paper, Rock Sciccors"
+puts "Welcome to Paper, Rock Scissors"
+puts "Please select what you want to use"
+puts "*********************************"
+puts "1. Rock"
+puts "2. Paper"
+puts "3. Scissors"
+puts "What is your choice?"
+choice = gets.chomp.to_i
+if choice == 1
+  puts "You selected Rock"
+elsif choice == 2
+  puts "you selected Paper"
+elsif choice == 3
+  puts "you selected Scissors"
+end 
+  
 
 def rock
   if rand(3) == 1 
