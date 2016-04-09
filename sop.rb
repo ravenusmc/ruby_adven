c9a23bce93cfd03474972e7d8faed35673652fc8
ravenusmc <mcuddy77@gmail.com>
2016-04-09 09:55:32 -0400

    more working on search methods in arrays

--- sop.rb
+++ sop.rb
@@ -13,24 +13,37 @@
 # puts aver([1,2,3,4])
 
 #How to find a specific value in an array 
-array = [200,3,500,150]
+# array = [200,3,500,150,7,8,134,234,56,77]
 
-num = 150
+# num = 8
 
-found = false 
+# found = false 
 
-index = 0
+# index = 0
 
-while found == false && index <= array.length 
-  if array[index] == num 
-    found == true 
-  else 
-    index = index + 1
-    puts "Your value was found at #{array[index]}"
+# puts "What value do you want to look for?"
+# index = gets.chomp.to_i
+
+# while found == false && index <= array.length - 1 
+#   if array[index] == num 
+#     found == true 
+#   else 
+#     index = index + 1
+#     puts "Your value of #{num} was found at position #{array[index]}"
+#   end 
+# end 
+
+#totaling the values in an array
+
+def aver(array)
+  total = 0 
+  array.each do |f|
+    total = total + f
   end 
+  return total
 end 
 
-
+puts aver([1,2,3,4,2])
 
 
 
