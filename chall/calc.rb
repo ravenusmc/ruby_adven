19a3c90f7701be285583d9ccf6eeec8f5271cfd2
ravenusmc <mcuddy77@gmail.com>
2016-04-27 14:01:30 -0400

    working on tip calculator

---
+++ chall/calc.rb
@@ -0,0 +1,8 @@
+#First Challenge in my exercises for programmers book. This is the description: 
+#Create a simple tip calculator. The program should prompt for a bill amount and a tip rate.
+#The program must compute the tip and then display both the tip and the total amount of the bill. 
+
+#Input: bill amount, tip rate 
+#process: calculate the tip based on the bill amount. Add the tip amount to the bill amount for a total.
+#Output: Total, tip amount. 
+
