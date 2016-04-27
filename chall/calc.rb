7e172e5a2e7353f6a3dcd56400175986334e98bc
ravenusmc <mcuddy77@gmail.com>
2016-04-27 14:06:10 -0400

    finished tip calculator

--- chall/calc.rb
+++ chall/calc.rb
@@ -6,3 +6,13 @@
 #process: calculate the tip based on the bill amount. Add the tip amount to the bill amount for a total.
 #Output: Total, tip amount. 
 
+puts "Please enter the bill amount:"
+bill = gets.chomp.to_f
+puts "Please enter the tip amount: "
+tip = gets.chomp.to_f
+
+tipAmount = bill * tip 
+total = bill + tipAmount 
+
+puts "Your total bill is #{total} which is based off of the #{tipAmount} tip amount!"
+puts "Please shop with us again!"
