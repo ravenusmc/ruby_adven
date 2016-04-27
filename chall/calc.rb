8d4ecfde3c9155d75255d65b299c6cdcd3701436
ravenusmc <mcuddy77@gmail.com>
2016-04-27 15:25:17 -0400

    added validation

--- chall/calc.rb
+++ chall/calc.rb
@@ -6,8 +6,23 @@
 #process: calculate the tip based on the bill amount. Add the tip amount to the bill amount for a total.
 #Output: Total, tip amount. 
 
+def valid(bill)
+    if bill < 0 
+      return false 
+    else 
+      return true 
+    end 
+end 
+
+total = 0
+tipAmount = 0
+
 puts "Please enter the bill amount:"
 bill = gets.chomp.to_f
+while !valid(bill)
+  puts "Please enter a number greater than 0"
+  bill = gets.chomp.to_f
+end 
 puts "Please enter the tip amount: "
 tip = gets.chomp.to_f
 
@@ -16,3 +31,10 @@ total = bill + tipAmount
 
 puts "Your total bill is #{total} which is based off of the #{tipAmount} tip amount!"
 puts "Please shop with us again!"
+
+#Basic warm up excerise from book. Create a program that asks the user for their name and then 
+#greets them. 
+
+# puts "What is your name?"
+# name = gets.chomp
+# puts "Hello, #{name}, nice to meet you!"
