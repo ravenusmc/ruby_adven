633fb3cbcd6e47324561dc90339ef6eb70b86738
ravenusmc <mcuddy77@gmail.com>
2016-04-27 15:28:13 -0400

    added a new file

---
+++ chall/count.rb
@@ -0,0 +1,5 @@
+#Create a program that prompts for an input string and displays out that shows the input string
+#and the number of characters the string contains. 
+
+#Example: What is the input string? Homer => Homer has 5 characters. 
+
